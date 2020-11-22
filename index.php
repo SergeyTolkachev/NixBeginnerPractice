bf47b5faa0668a2c99a8acc7ac0a07fbf36e898d
Sergey Tolkachev <sergei.tolkachev@ukr.net>
2020-11-22 19:05:41 +0200

    3 step of practice. Beginner

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+</head>
+<body>
+    <header>
+    </header>
 
-echo "Hello, NIX Education";
+    <nav>
+        <a href="#">Lesson 1</a> ||
+        <a href="#">Lesson 2</a>
+    </nav>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+</html>
